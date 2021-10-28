89da57cd138a127659a7691c7665396f24d4d152
sankaire <kerenketepela@gmail.com>
2021-10-28 10:46:52 +0300

    hello world

---
+++ 00_syntax.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+<!--Echo HTML tags-->
+<?php
+  echo"hello world, from php";
+  echo "And Peter";
+  echo "<br>";
+  echo '<button> Click me </button>';
+?>
+
+<?php
+  echo '<br>';
+  echo 'hello from another tag'
+?>
+</body>
+</html>
