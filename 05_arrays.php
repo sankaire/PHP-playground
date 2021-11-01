dfc82b188a65a12ac9d30facb1101792eba3d270
sankaire <kerenketepela@gmail.com>
2021-11-01 13:36:32 +0300

    arrays

--- 05_arrays.php
+++ 05_arrays.php
@@ -36,27 +36,76 @@ echo '</pre>';
 
 // Print the length of the array
 
-
+echo count($fruits). '<br>';
 
 // Add element at the end of the array
 
+array_push($fruits, 'fooo');
+echo '<pre>';
+var_dump($fruits);
+echo '<pre>';
+
 // Remove element from the end of the array
+
+array_pop($fruits);
+
+echo '<pre>';
+var_dump($fruits);
+echo '<pre>';
 
 // Add element at the beginning of the array
 
+array_unshift($fruits, 'fsfsf');
+
+echo '<pre>';
+var_dump($fruits);
+echo '<pre>';
 // Remove element from the beginning of the array
+
+array_shift($fruits);
+echo '<pre>';
+var_dump($fruits);
+echo '<pre>';
 
 // Split the string into an array
 
+$string = "banana, orange, mongo";
+
+echo '<pre>';
+var_dump(explode(',', $string));
+echo '<pre>';
+
 // Combine array elements into string
+
+echo implode('$', $fruits). '<br>';
 
 // Check if element exist in the array
 
+echo '<pre>';
+var_dump(in_array('Mongo', $fruits));
+echo '<pre>';
+
 // Search element index in the array
+
+echo '<pre>';
+var_dump(array_search('Mongo', $fruits));
+echo '<pre>';
 
 // Merge two arrays
 
+$vegetables = ['sukumawiki', 'cucumber'];
+
+echo '<pre>';
+var_dump(array_merge($fruits, $vegetables));
+echo '<pre>';
+
+
 // Sorting of array (Reverse order also)
+
+sort($fruits);
+ echo '<pre>';
+ var_dump($fruits);
+ echo '<pre>';
 
 
 // https://www.php.net/manual/en/ref.array.php
@@ -67,19 +116,67 @@ echo '</pre>';
 
 // Create an associative array
 
+$person = [
+    'fname' => 'Peter',
+    'sname' => 'Tepela',
+    'age' => 21,
+    'hobbies' => ['soccer', 'basketball', 'swimming'],
+];
+echo '<pre>';
+print_r($person);
+echo '<pre>';
+
 // Get element by key
+
+echo $person['fname']. '<br>';
 
 // Set element by key
 
+$person['company'] = 'Qwetu inc,';
+echo '<pre>';
+print_r($person);
+echo '<pre>';
+
+
 // Null coalescing assignment operator. Since PHP 7.4
 
+if(!isset($person['address'])){
+    $person['address'] = '000 newel road';
+}
+
+echo '<pre>';
+print_r($person);
+echo '<pre>';
+
+
 // Check if array has specific key
-
+ 
+ 
 // Print the keys of the array
-
+echo '<pre>';
+print_r(array_keys($person));
+echo '<pre>';
 // Print the values of the array
-
+echo '<pre>';
+print_r(array_values($person));
+echo '<pre>';
 // Sorting associative arrays by values, by keys
-
-
+ksort($person);
+echo '<pre>';
+print_r(($person));
+echo '<pre>';
 // Two dimensional arrays
+
+$todos = [
+    [
+        'title' => 'todo title 1',
+        'comleted' => true,
+    ],
+    [
+        'title' => 'todo title 2',
+        'comleted' => false,
+    ],
+];
+echo '<pre>';
+print_r(($todos));
+echo '<pre>';
