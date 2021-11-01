9eb968433075f07ad29838c2542ce93e8b15ea27
sankaire <kerenketepela@gmail.com>
2021-11-01 13:35:48 +0300

    loops

--- 07_loops.php
+++ 07_loops.php
@@ -1,13 +1,58 @@
 <?php
 
-// while
-
+// while loops
+ 
+// while(/*condition*/){
+//     //excecution;
+// };
+ 
 // Loop with $counter
-
+ 
+$counter = 0;
+while($counter < 10){
+    echo $counter. '<br>';
+    if($counter === 5){
+        break;
+    }
+    $counter++;
+}
+ 
 // do - while
 
-// for
+do{
+    echo $counter. '<br>';
+    $counter ++;
+} while($counter < 20);
 
-// foreach
+// for loop
 
+for($j = 0; $j < 10; $j++){
+    echo $j. '<br>';
+}
+ 
+// foreach 
+
+$fruits = ['Apple', 'Banana', 'Mongo', 'Berries'];
+
+foreach($fruits as$i => $fruit){
+    echo $i. $fruit. '<br>';
+}
+ 
 // Iterate Over associative array.
+
+$person =[
+    'fname' => 'Peter',
+    'sname' => 'Tepela',
+    'hobies' => [
+        'soccer', 'swimming', 'coding',
+    ],
+];
+
+foreach($person as $key => $value){
+
+    if(is_array($value)){
+        echo $key.' '.implode(',', $value). '<br>';
+    }else{
+        echo $key.' '.$value . '<br>';
+    }
+}
