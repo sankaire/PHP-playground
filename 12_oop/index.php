762e589b3f4c990c4631e80e79b5d369dce721f3
sankaire <kerenketepela@gmail.com>
2021-11-02 16:43:52 +0300

    OOP

--- 12_oop/index.php
+++ 12_oop/index.php
@@ -2,7 +2,28 @@
 
 // What is class and instance
 
+class Person{
+    //properties
+    public $age;
+    public $country;
+
+    public function id($age){
+        if($age < 18){
+            echo 'You are not an adult!!';
+        }
+    }
+
+}
+
+$Peter = new Person;
+
+$Peter -> $age = 21;
+$Peter -> $country = 'Kenya';
+$Peter -> id(18);
+
 // Create Person class in Person.php
+
+
 
 // Create instance of Person
 
