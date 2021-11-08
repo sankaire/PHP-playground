33d31bcb908d1ae3668b7104f7d4fdcd9a35018c
sankaire <kerenketepela@gmail.com>
2021-11-08 20:22:24 +0300

    math module

---
+++ 10_include_require/02_functions/math.php
@@ -0,0 +1,16 @@
+<?php
+function add ($a, $b){
+    return $a + $b;
+}
+
+function sub ($a, $b){
+    return $a - $b;
+}
+
+function div ($a, $b){
+    return $a / $b;
+}
+
+function mult ($a, $b){
+    return $a * $b;
+}
